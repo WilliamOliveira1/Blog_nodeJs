ee9e0368bcba3d93e1833d784efedde32877cc3e
WilliamOliveira1 <1704639@aluno.univesp.br>
2021-06-19 20:19:49 -0300

    set database and static files

---
+++ index.js
@@ -0,0 +1,38 @@
+// importando o express
+const express = require('express');
+const app = express();
+
+// importando a connection do banco de dados
+const connection = require("./database/database");
+
+// Conectar com o banco de dados
+connection
+  .authenticate()
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((msgErro) => {
+    console.log(msgErro);
+  });
+
+
+//Setar o EJS para renderizar o HTML
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+// Configurar o body-parser
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Rotas
+app.get("/", (req, res) => {
+      res.render("index");
+});
+
+app.listen(8080, (error) => {
+  if(error) {
+    console.error("The server caught an error: " + error);
+  }else {
+    console.log("The server is running");
+  }
+});
