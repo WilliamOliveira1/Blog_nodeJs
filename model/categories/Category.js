ea3370dfdea928a6fc344cdbefe693b3fff6c2ae
WilliamOliveira1 <1704639@aluno.univesp.br>
2021-06-19 21:31:10 -0300

    set model Category

---
+++ model/categories/Category.js
@@ -0,0 +1,16 @@
+// Importar o sequelize
+const Sequelize = require('sequelize');
+const connection = require("../../database/database");
+
+const Category = define('categories', {
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    slug: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = Category;
